e67772f91e1ac252d86398703260ce8d91fb5e96
FR060 <lr.aodencia@mmdc.mcl.edu.ph>
2025-11-06 00:54:44 +0800

    Delete src directory

--- src/main/java/com/mycompany/terminalassess/TerminalAssess.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.terminalassess;
-
-import javax.swing.SwingUtilities;
-
-public class TerminalAssess {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new LoginFrame());
-    }
-}
