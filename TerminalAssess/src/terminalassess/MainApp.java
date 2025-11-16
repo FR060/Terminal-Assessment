0a83e8ef502782a53d807e185a34f4e2bb4d1b51
FR060 <lr.aodencia@mmdc.mcl.edu.ph>
2025-11-17 01:18:23 +0800

    Delete TerminalAssess directory

--- TerminalAssess/src/terminalassess/MainApp.java
+++
@@ -1,9 +0,0 @@
-package terminalassess;
-
-public class MainApp {
-    public static void main(String[] args) {
-        FileInitializer.ensureFilesExist();
-
-        javax.swing.SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
-    }
-}
